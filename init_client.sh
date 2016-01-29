ccb52d50410e24dbd490aaaa49e446abdf6606ad
Gang Li <leemagpie@gmail.com>
2016-01-29 13:52:56 -0500

    Rearrage steps.

--- init_client.sh
+++ init_client.sh
@@ -5,7 +5,7 @@ CWD=$PWD
 echo $CWD
 mkdir dep
 
-# Install protobuf dependencies.
+# Download packages from Github.
 # https://github.com/google/protobuf/releases
 cd ${CWD}/dep
 rm -rf protobuf
@@ -13,22 +13,6 @@ git clone https://github.com/google/protobuf
 cd protobuf
 git checkout tags/v3.0.0-beta-1
 
-# Generate configuration files
-./autogen.sh
-./configure
-make
-make check
-sudo make install
-
-# To delete the system-level installation
-# make uninstall
-
-# Install python dependencies.
-cd python
-python setup.py install
-
-
-# Install grpc dependencies.
 # The C++ based grpc stack, including python and other languages.
 # https://github.com/grpc/grpc
 cd ${CWD}/dep
@@ -37,8 +21,28 @@ git clone https://github.com/grpc/grpc.git
 cd grpc
 git checkout tags/release-0_12_0
 git submodule update --init
-make
-sudo make install
 
-# Install grpc python library.
+# Make packages.
+# Make protobuf.
+# Generate configuration files
+cd ${CWD}/dep/protobuf
+./autogen.sh
+./configure
+make
+make check
+# sudo make install
+# To delete the system-level installation
+# make uninstall
+
+# Make grpc.
+cd ${CWD}/dep/grpc
+make
+#sudo make install
+
+# Install Python modules.
+# Protobuf Python module.
+cd ${CWD}/dep/protobuf/python
+python setup.py install
+
+# Grpc python library.
 pip install grpcio
