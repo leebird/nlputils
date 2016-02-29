b14c7a3c95a68e23ffcbc8248a70dc4dad94f326
Gang Li <leemagpie@gmail.com>
2016-02-29 16:19:55 -0500

    Separate data and rpc

--- scripts/compile_document_proto.sh
+++ scripts/compile_document_proto.sh
@@ -2,8 +2,12 @@
 set -e
 
 # Create folders.
+rm -r ../protolib/python
+rm -r ../protolib/java
 mkdir -p ../protolib/python
 mkdir -p ../protolib/java
+touch ../protolib/python/__init__.py
+touch ../protolib/__init__.py
 
 # Compile python codes.
 protoc --proto_path=../proto --python_out=../protolib/python --grpc_out=../protolib/python --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../proto/document.proto
@@ -11,4 +15,13 @@ protoc --proto_path=../proto --python_out=../protolib/python --grpc_out=../proto
 # Compile java codes.
 protoc --plugin=protoc-gen-grpc-java=../dep/grpc-java/compiler/build/binaries/java_pluginExecutable/protoc-gen-grpc-java \
 --proto_path=../proto --grpc-java_out=../protolib/java --java_out=../protolib/java ../proto/document.proto
-javac -classpath "../dep/protobuf/java/target/protobuf-java-3.0.0-beta-1.jar" ../protolib/java/edu/delaware/nlp/Protobuf.java
+javac -classpath "../dep/protobuf/java/target/protobuf-java-3.0.0-beta-1.jar" ../protolib/java/edu/delaware/nlp/DocumentProto.java
+
+# Compile rpc proto.
+# Compile python codes.
+protoc --proto_path=../proto --python_out=../protolib/python --grpc_out=../protolib/python --plugin=protoc-gen-grpc=`which grpc_python_plugin` ../proto/rpc.proto
+
+# Compile java codes.
+protoc --plugin=protoc-gen-grpc-java=../dep/grpc-java/compiler/build/binaries/java_pluginExecutable/protoc-gen-grpc-java \
+--proto_path=../proto --grpc-java_out=../protolib/java --java_out=../protolib/java ../proto/rpc.proto
+javac -classpath "../dep/protobuf/java/target/protobuf-java-3.0.0-beta-1.jar:../protolib/java/" ../protolib/java/edu/delaware/nlp/RpcProto.java
