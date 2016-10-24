4887d027bfe322c942ad413e1de4889205667798
Gang Li <leemagpie@gmail.com>
2016-10-24 16:55:34 -0400

    Solve BLLIP memory leak issue

--- server/bllip/bllip_parser.py
+++ server/bllip/bllip_parser.py
@@ -1,37 +1,51 @@
-from bllipparser import Sentence, RerankingParser
+# coding=utf-8
 import os
+import bllipparser
+import glog
 
 # NOTE that this BLLIP parser is not thead-safe. See
 # https://github.com/dmcc/bllip-parser/blob/master/python/bllipparser/RerankingParser.py#L537
-# add first stage's README. So we can't use BLLIP parser
-# in multi-threads. See bllip_process_pool.py for solution.
+# and first stage's README. So we can't use BLLIP parser in multi-threads. See
+# bllip_process_pool.py for solution.
 
 
 class BllipParser(object):
-    def __init__(self):
-        curr_path = os.path.realpath(__file__)
-        model_dir = os.path.join(os.path.dirname(curr_path), 'model')
-        
-        self.rrp = RerankingParser.fetch_and_load('GENIA+PubMed',
-                                                  models_directory=model_dir,
-                                                  verbose=False)
+    def __init__(self, model_dir=None):
+        """ Init the parser given the model folder.
+        :param model_dir: Path to the folder storing the model, downloaded by
+        RerankingParser.fetch_and_load() method.
+        :type model_dir: str
+        """
+        if model_dir is None:
+            # Use 'model/GENIA+PubMed' under current folder as default.
+            filepath = os.path.realpath(__file__)
+            dirpath = os.path.dirname(filepath)
+            model_dir = os.path.join(dirpath, 'model/GENIA+PubMed')
+        glog.setLevel(glog.INFO)
+        glog.info('Loading model from ' + model_dir)
+
+        # BLLIP parser doesn't support multi-thread parsing, and we will
+        # run it as one thread in one process (in both nlputils and spark),
+        # so this should be fine. It will raise a RunTimeError when initialize
+        # twice.
+        self.reranking_parser = \
+            bllipparser.RerankingParser.from_unified_model_dir(model_dir)
 
     def parse_one_sentence(self, text):
         if type(text) is not unicode:
             raise ValueError('Input must be unicode string')
-        sentence = Sentence(text.encode('utf8'))
-        # If any error happens, just return None.
+        sentence = bllipparser.Sentence(text.encode('utf8'))
         try:
-            parse_tree = self.rrp.simple_parse(sentence)
+            parse_tree = self.reranking_parser.simple_parse(sentence)
             return parse_tree.decode('utf8')
-        except:
-            return None
+        except Exception as e:
+            # If any error happens, just raise an RunTimeError.
+            raise RuntimeError("Parsing failed: {0}".format(e))
 
-    '''
-    def tag_one_sentence(self, text):
-        if type(text) is not unicode:
-            raise ValueError('Input must be unicode string')
-        sentence = Sentence(text.encode('utf8'))
-        tags = self.rrp.tag(sentence)
-        return tags
-    '''
+
+if __name__ == '__main__':
+    parser = BllipParser('./model/GENIA+PubMed')
+    parse = parser.parse_one_sentence(u"I have a book")
+    print(parse)
+    parse = parser.parse_one_sentence(u"你好，test utf8字符串。")
+    print(parse.encode('utf8'))
--- server/bllip/bllip_process_pool.py
+++ server/bllip/bllip_process_pool.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals, print_function
 from multiprocessing import Queue, Process, current_process
+from threading import Lock
 import random
 from bllip_parser import BllipParser
 import glog
+from time import sleep
+import resource
+
 
 # Since BLLIP parser is not thread-safe, we use process to do parallel parsing.
 # We create a pool of BLLIP parsers, each parser is held by an indepdendent
@@ -36,7 +40,7 @@ def _bllip_parser_worker(name, input_queue, output_queue):
     # Maybe because it is a different process and the logging module is
     # not supported well across processes.
     worker_process = name + '_' + current_process().name
-    glog.info('Loading BLLIP parser...')
+    glog.info('Loading BLLIP parser: ' + worker_process)
     parser = BllipParser()
     output_queue.put(True)
     glog.info('BLLIP parser loaded')
@@ -52,37 +56,85 @@ def _bllip_parser_worker(name, input_queue, output_queue):
                 # loop. 3605188/communicating-end-of-queue on StackOverflow
                 # for details.
                 break
-            parse_tree = parser.parse_one_sentence(sentence)
-            output_queue.put(parse_tree)
+            try:
+                parse_tree = parser.parse_one_sentence(sentence)
+            except RuntimeError:
+                # Catch RuntimeError but not ValueError (caused by non-unicode
+                # string).
+                output_queue.put(None)
+            else:
+                output_queue.put(parse_tree)
         except (IOError, KeyboardInterrupt):
-            glog.info('BLLIP parser exits due to exception')
+            # Unrecoverable errors.
+            glog.info('BLLIP parser {} exits due to unrecoverable '
+                      'exception'.format(worker_process))
             break
+
+    # Output peak memory at the end.
+    glog.info('Peak memory usage of {}: {}'.format(
+        worker_process,
+        resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024))
 
 
 class BllipWorker(object):
     def __init__(self, name):
         self.name = name
+        # If there are more than 50 sentences, then block the input queue.
+        self.inqueue = Queue()
 
-        # If there are more than 50 sentences, then block the input queue.        
-        self.inqueue = Queue(50)
         # Output queue is associated with input queue.
         self.dequeue = Queue()
+
+        # A lock to ensure input/output matching in multi-threading.
+        self.lock = Lock()
+
+        # Counter of parsed sentences.
+        self.parsed_count = 0
+
+        # At most parse this many sentences for one process.
+        self.max_sentence_limit = 5000
+
         # Start the corresponding worker in a new process.
         glog.info('Starting BLLIP worker...')
         self.init_worker()
         glog.info('BLLIP worker loaded')
 
     def init_worker(self):
-        self.worker = Process(target=_bllip_parser_worker, 
+        self.worker = Process(target=_bllip_parser_worker,
                               args=(self.name, self.inqueue, self.dequeue))
         self.worker.start()
         # Block until the parser is inited. The parser will put a value
         # to dequeue after it is inited.
         init_message = self.dequeue.get()
 
-    def parse_one_sentence(self, sentence):
+    def _parse_one_sentence(self, sentence):
         self.inqueue.put(sentence)
-        return self.dequeue.get(sentence)
+        # sleep(random.random())
+        parse = self.dequeue.get()
+        self.parsed_count += 1
+        return parse
+
+    def parse_one_sentence(self, sentence):
+        # There may be multiple threads accessing this function from the
+        # BllipManager. Use lock to ensure input/output matching.
+        self.lock.acquire()
+        if self.parsed_count >= self.max_sentence_limit:
+            # Create a new parser process after 5000 sentences.
+            glog.info(self.name + ' reached max sentence limit')
+            glog.info('Shutting down ' + self.name)
+            self.stop()
+            self.init_worker()
+
+        # Parse the sentence.
+        parse = self._parse_one_sentence(sentence)
+
+        # If parse is None, something wrong in the parser, probably caused
+        # by the sentence. Restart the parser and return the None result.
+        if parse is None:
+            self.stop()
+            self.init_worker()
+        self.lock.release()
+        return parse
 
     def stop(self):
         self.inqueue.put(None)
@@ -99,12 +151,12 @@ class BllipManager(object):
 
     def create_parser_pool(self):
         for i in range(self.process_pool_size):
-            name = 'BllipWorker-{}'.format(i+1)
+            name = 'BllipWorker-{}'.format(i + 1)
             self.pool.append(BllipWorker(name))
 
     def parse_one_sentence(self, sentence):
         try:
-            selected_worker = random.randint(0, self.process_pool_size-1)
+            selected_worker = random.randint(0, self.process_pool_size - 1)
             parse = self.pool[selected_worker].parse_one_sentence(sentence)
             return parse
         except Exception as e:
@@ -114,3 +166,21 @@ class BllipManager(object):
         for worker in self.pool:
             worker.stop()
 
+
+if __name__ == '__main__':
+    # If not use lock, the input/ouput will mismatch. Run this following
+    # code to see an example. Uncomment the sleep() line and the lock-related
+    # lines in BllipWorker.parse_one_sentence().
+    from multiprocessing.pool import ThreadPool
+
+    glog.setLevel(glog.INFO)
+    manager = BllipManager(1)
+    pool = ThreadPool(10)
+    pool.map(lambda sentence: glog.info(
+        sentence + ": " + manager.parse_one_sentence(sentence)),
+             ['I have a book 1.', 'I have a book 2.',
+              'I have a book 3.', 'I have a book 4.',
+              'I have a book 5.', 'I have a book 6.',
+              'I have a book 7.', 'I have a book 8.',
+              'I have a book 9.', 'I have a book 10.'])
+    manager.stop()
