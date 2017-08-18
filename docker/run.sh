a2131399823c3e1829bb6e08ac3713a37498dbe9
Gang Li <leemagpie@gmail.com>
2017-08-18 12:01:39 -0400

    Add pull to docker run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # BLLIP_PROC_NUM is the number of bllip parser processes.
-docker run --init --network=host -e "BLLIP_PROC_NUM=1" leebird/nlputils
+docker pull leebird/nlputils
+docker run --init --network=host -d -e "BLLIP_PROC_NUM=1" leebird/nlputils
