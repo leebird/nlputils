7e7debf236c6630306390e5d44d285eae4fe22e4
Gang Li <leemagpie@gmail.com>
2019-06-23 18:08:28 -0400

    Update run.sh for docker

--- docker/run.sh
+++ docker/run.sh
@@ -3,9 +3,14 @@
 # To clean unused volumns: docker volume rm $(docker volume ls -qf dangling=true)
 # To see current container logs (useful for checking if the server is ready): docker logs CONTAINER_ID
 
-# BLLIP_PROC_NUM is the number of bllip parser processes.
+# BLLIP_PROC_NUM: Number of BLLIP parser processes for parallel processing.
+# BLLIP_PORT: BLLIP parser server port.
+# STANFORD_VERSION: Stanford CoreNLP version.
+# STANFORD_PORT: Stanford CoreNLP server port (main port used by the client codes)
+# VISUAL_PORT: Visual web interface port.
+
 docker pull leebird/nlputils
-docker run --init --network=host -d \
+docker run --name=nlputils_server --init --network=host -d \
        -e BLLIP_PROC_NUM=10 \
        -e BLLIP_PORT=8901 \
        -e STANFORD_VERSION=3.6.0 \
